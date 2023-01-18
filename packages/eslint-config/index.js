19fc3a13f104fbdfdb42446d0f7312ca2d825567
kaivanwong <kaivanwong@outlook.com>
2023-01-18 09:50:53 +0800

    feat: add eslint-config-vue

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,14 +1,21 @@
 const { isPackageExists } = require('local-pkg')
 
-const TS = isPackageExists('typescript')
+const Extends = []
 
-if (!TS)
+const TS = isPackageExists('typescript')
+const VUE = isPackageExists('vue')
+
+if (TS) {
+  Extends.push('@kaivanwong/eslint-config-ts')
+}
+else {
+  Extends.push('@kaivanwong/eslint-config-base')
   console.warn('[@kaivanwong/eslint-config] TypeScript is not installed, fallback to JS only.')
+}
+
+if (VUE)
+  Extends.push('@kaivanwong/eslint-config-vue')
 
 module.exports = {
-  extends: [
-    TS
-      ? '@kaivanwong/eslint-config-ts'
-      : '@kaivanwong/eslint-config-base',
-  ],
+  extends: Extends,
 }
