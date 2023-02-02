4a0bfeff111b4a585f3871708311fcb6b92639cb
kaivanwong <kaivanwong@outlook.com>
2023-02-02 15:29:51 +0800

    feat: add eslint-config-react

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -4,6 +4,7 @@ const Extends = []
 
 const TS = isPackageExists('typescript')
 const VUE = isPackageExists('vue')
+const REACT = isPackageExists('react')
 const NEST = isPackageExists('nestjs')
 
 if (TS) {
@@ -16,6 +17,9 @@ else {
 
 if (VUE)
   Extends.push('@kaivanwong/eslint-config-vue')
+
+if (REACT)
+  Extends.push('@kaivanwong/eslint-config-react')
 
 if (NEST)
   Extends.push('@kaivanwong/eslint-config-nest')
