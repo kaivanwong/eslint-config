9b68a8feb6ef6384a198393b11e955dd46662a5b
kaivanwong <kaivanwong@outlook.com>
2023-02-02 16:02:46 +0800

    feat: add react rules

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,5 +1,13 @@
 module.exports = {
   extends: [
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
   ],
+  rules: {
+    'jsx-quotes': [
+      'error',
+      'prefer-double',
+    ],
+    'react/react-in-jsx-scope': 'off',
+  },
 }
