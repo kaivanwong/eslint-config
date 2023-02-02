ce4c02ccee6626b6dc3d9afcd23940083831f81e
kaivanwong <kaivanwong@outlook.com>
2023-02-02 15:48:18 +0800

    feat: update nest config

--- packages/eslint-config-nest/index.js
+++ packages/eslint-config-nest/index.js
@@ -1,5 +1,12 @@
 module.exports = {
+  overrides: [
+    {
+      files: ['*.ts'],
+      rules: {
+        '@typescript-eslint/consistent-type-imports': 'off',
+      },
+    },
+  ],
   rules: {
-    '@typescript-eslint/consistent-type-imports': 'off',
   },
 }
