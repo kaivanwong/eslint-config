a4c7fe29f62e9241a100b091b2df176c716a1c2a
kaivanwong <kaivanwong@outlook.com>
2023-02-03 13:40:36 +0800

    feat: add custome rules

--- packages/eslint-config-nest/index.js
+++ packages/eslint-config-nest/index.js
@@ -1,13 +1,5 @@
 module.exports = {
-  extends: ['plugin:@darraghor/nestjs-typed/recommended'],
-  overrides: [
-    {
-      files: ['*.ts'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        project: ['./tsconfig.json'],
-      },
-    },
-  ],
-  plugins: ['@darraghor/nestjs-typed'],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'off',
+  },
 }
