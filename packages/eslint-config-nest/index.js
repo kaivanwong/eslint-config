5b7e36fb178862b0772b17676ecef511fdb6a8ff
kaivanwong <kaivanwong@outlook.com>
2023-02-03 13:36:48 +0800

    feat: update config

--- packages/eslint-config-nest/index.js
+++ packages/eslint-config-nest/index.js
@@ -1,4 +1,13 @@
 module.exports = {
   extends: ['plugin:@darraghor/nestjs-typed/recommended'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  ],
   plugins: ['@darraghor/nestjs-typed'],
 }
