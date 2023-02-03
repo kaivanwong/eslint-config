cc294090b767f59a17f979301d955f4f2da69922
kaivanwong <kaivanwong@outlook.com>
2023-02-03 11:40:03 +0800

    feat: remove parserOptions

--- packages/eslint-config-nest/index.js
+++ packages/eslint-config-nest/index.js
@@ -1,10 +1,4 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['./tsconfig.json'],
-    sourceType: 'module',
-    ecmaVersion: 'es2019',
-  },
   extends: ['plugin:@darraghor/nestjs-typed/recommended'],
   plugins: ['@darraghor/nestjs-typed'],
 }
