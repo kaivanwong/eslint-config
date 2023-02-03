e9f6dec774bf9ec406171635bd076920896fb2e0
kaivanwong <kaivanwong@outlook.com>
2023-02-03 11:25:05 +0800

    feat: add parserOptions

--- packages/eslint-config-nest/index.js
+++ packages/eslint-config-nest/index.js
@@ -1,5 +1,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'es2019',
+  },
   extends: ['plugin:@darraghor/nestjs-typed/recommended'],
   plugins: ['@darraghor/nestjs-typed'],
 }
