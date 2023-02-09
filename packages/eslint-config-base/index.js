37e265b515c83422db3b835025b0c72a51b47c0c
kaivanwong <kaivanwong@outlook.com>
2023-02-09 15:14:35 +0800

    feat: update js & ts config

--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -4,11 +4,15 @@ module.exports = {
     browser: true,
     node: true,
   },
-  settings: {
-    'import/resolver': {
-      node: { extensions: ['.js', '.mjs'] },
-    },
-  },
+  reportUnusedDisableDirectives: true,
+  extends: [
+    './standard',
+    'plugin:import/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:jsonc/recommended-with-jsonc',
+    'plugin:yml/standard',
+    'plugin:markdown/recommended',
+  ],
   ignorePatterns: [
     '*.min.*',
     '*.d.ts',
@@ -24,25 +28,21 @@ module.exports = {
     'pnpm-lock.yaml',
     'yarn.lock',
     '__snapshots__',
-    '.github',
-    '.vitepress',
-    '.vscode',
-    '.husky',
-    '.netlify',
-  ],
-  extends: [
-    './standard',
-    'plugin:import/recommended',
-    'plugin:eslint-comments/recommended',
-    'plugin:jsonc/recommended-with-jsonc',
-    'plugin:yml/standard',
-    'plugin:markdown/recommended',
+    '!.github',
+    '!.vitepress',
+    '!.vscode',
   ],
   plugins: [
     'html',
     'unicorn',
     'no-only-tests',
+    'unused-imports',
   ],
+  settings: {
+    'import/resolver': {
+      node: { extensions: ['.js', '.mjs'] },
+    },
+  },
   overrides: [
     {
       files: ['*.json', '*.json5'],
@@ -142,7 +142,7 @@ module.exports = {
       },
     },
     {
-      files: ['*.js'],
+      files: ['*.js', '*.cjs'],
       rules: {
         '@typescript-eslint/no-var-requires': 'off',
       },
@@ -166,6 +166,7 @@ module.exports = {
         'no-only-tests/no-only-tests': 'error',
       },
     },
+    // Code blocks in markdown file
     {
       files: ['**/*.md/*.*'],
       rules: {
@@ -176,6 +177,8 @@ module.exports = {
         '@typescript-eslint/comma-dangle': 'off',
         '@typescript-eslint/consistent-type-imports': 'off',
         'import/no-unresolved': 'off',
+        'unused-imports/no-unused-imports': 'off',
+        'unused-imports/no-unused-vars': 'off',
         'no-alert': 'off',
         'no-console': 'off',
         'no-restricted-imports': 'off',
@@ -186,16 +189,25 @@ module.exports = {
     },
   ],
   rules: {
+    // import
     'import/order': 'error',
     'import/first': 'error',
     'import/no-mutable-exports': 'error',
     'import/no-unresolved': 'off',
     'import/no-absolute-path': 'off',
+
+    // Common
     'semi': ['error', 'never'],
     'curly': ['error', 'multi-or-nest', 'consistent'],
     'quotes': ['error', 'single'],
     'quote-props': ['error', 'consistent-as-needed'],
-    'no-unused-vars': 'warn',
+
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
+
     'no-param-reassign': 'off',
     'array-bracket-spacing': ['error', 'never'],
     'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
@@ -227,6 +239,8 @@ module.exports = {
         asyncArrow: 'always',
       },
     ],
+
+    // es6
     'no-var': 'error',
     'prefer-const': [
       'error',
@@ -268,6 +282,8 @@ module.exports = {
         balanced: true,
       },
     }],
+
+    // best-practice
     'array-callback-return': 'error',
     'block-scoped-var': 'error',
     'consistent-return': 'off',
@@ -285,23 +301,38 @@ module.exports = {
     'no-return-assign': 'off',
     'operator-linebreak': ['error', 'before'],
     'max-statements-per-line': ['error', { max: 1 }],
+
+    // unicorns
+    // Pass error message when throwing errors
     'unicorn/error-message': 'error',
+    // Uppercase regex escapes
     'unicorn/escape-case': 'error',
+    // Array.isArray instead of instanceof
     'unicorn/no-instanceof-array': 'error',
+    // Prevent deprecated `new Buffer()`
     'unicorn/no-new-buffer': 'error',
+    // Keep regex literals safe!
     'unicorn/no-unsafe-regex': 'off',
+    // Lowercase number formatting for octal, hex, binary (0x1'error' instead of 0X1'error')
     'unicorn/number-literal-case': 'error',
+    // includes over indexOf when checking for existence
     'unicorn/prefer-includes': 'error',
+    // String methods startsWith/endsWith instead of more complicated stuff
     'unicorn/prefer-string-starts-ends-with': 'error',
+    // textContent instead of innerText
     'unicorn/prefer-text-content': 'error',
+    // Enforce throwing type error when throwing error while checking typeof
     'unicorn/prefer-type-error': 'error',
+    // Use new when throwing error
     'unicorn/throw-new-error': 'error',
+
     'no-use-before-define': ['error', { functions: false, classes: false, variables: true }],
     'eslint-comments/disable-enable-pair': 'off',
     'import/no-named-as-default-member': 'off',
     'import/no-named-as-default': 'off',
     'import/namespace': 'off',
     'n/no-callback-literal': 'off',
+
     'sort-imports': [
       'error',
       {
@@ -312,6 +343,8 @@ module.exports = {
         allowSeparatedGroups: false,
       },
     ],
+
+    // yml
     'yml/quotes': ['error', { prefer: 'single', avoidEscape: false }],
     'yml/no-empty-document': 'off',
   },
--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -5,69 +5,76 @@ const base = require('@kaivanwong/eslint-config-base')
 const tsconfig = process.env.ESLINT_TSCONFIG || 'tsconfig.eslint.json'
 
 module.exports = {
-  settings: {
-    'import/resolver': {
-      node: { extensions: ['.js', '.jsx', '.mjs', '.ts', '.tsx', '.d.ts'] },
-    },
-  },
   extends: [
     '@kaivanwong/eslint-config-base',
     'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
   ],
+  settings: {
+    'import/resolver': {
+      node: { extensions: ['.js', '.jsx', '.mjs', '.ts', '.tsx', '.d.ts'] },
+    },
+  },
   overrides: base.overrides.concat(
     !fs.existsSync(join(process.cwd(), tsconfig))
       ? []
       : [{
-          parserOptions: {
-            tsconfigRootDir: process.cwd(),
-            project: [tsconfig],
-          },
-          parser: '@typescript-eslint/parser',
-          excludedFiles: ['**/*.md/*.*'],
-          files: ['*.ts', '*.tsx', '*.mts', '*.cts'],
-          rules: {
-            'dot-notation': 'off',
-            'require-await': 'off',
-            'no-throw-literal': 'off',
-            'no-implied-eval': 'off',
-            '@typescript-eslint/no-throw-literal': 'error',
-            '@typescript-eslint/no-implied-eval': 'error',
-            '@typescript-eslint/dot-notation': ['error', { allowKeywords: true }],
-            '@typescript-eslint/no-floating-promises': 'error',
-            '@typescript-eslint/no-misused-promises': 'error',
-            '@typescript-eslint/await-thenable': 'error',
-            '@typescript-eslint/no-for-in-array': 'error',
-            '@typescript-eslint/no-unnecessary-type-assertion': 'error',
-            '@typescript-eslint/no-unsafe-argument': 'error',
-            '@typescript-eslint/no-unsafe-assignment': 'error',
-            '@typescript-eslint/no-unsafe-call': 'error',
-            '@typescript-eslint/no-unsafe-member-access': 'error',
-            '@typescript-eslint/no-unsafe-return': 'error',
-            '@typescript-eslint/require-await': 'error',
-            '@typescript-eslint/restrict-plus-operands': 'error',
-            '@typescript-eslint/restrict-template-expressions': 'error',
-            '@typescript-eslint/unbound-method': 'error',
-          },
-        }, {
-          files: ['**/__tests__/**/*.ts', '**/*.spec.ts', '**/*.test.ts'],
-          plugins: ['jest'],
-          rules: {
-            'jest/unbound-method': 'error',
-            '@typescript-eslint/unbound-method': 'off',
-          },
-        }],
+        parserOptions: {
+          tsconfigRootDir: process.cwd(),
+          project: [tsconfig],
+        },
+        parser: '@typescript-eslint/parser',
+        excludedFiles: ['**/*.md/*.*'],
+        files: ['*.ts', '*.tsx', '*.mts', '*.cts'],
+        // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended-requiring-type-checking.ts
+        rules: {
+          'no-throw-literal': 'off',
+          '@typescript-eslint/no-throw-literal': 'error',
+          'no-implied-eval': 'off',
+          '@typescript-eslint/no-implied-eval': 'error',
+          'dot-notation': 'off',
+          '@typescript-eslint/dot-notation': ['error', { allowKeywords: true }],
+          '@typescript-eslint/no-floating-promises': 'error',
+          '@typescript-eslint/no-misused-promises': 'error',
+          '@typescript-eslint/await-thenable': 'error',
+          '@typescript-eslint/no-for-in-array': 'error',
+          '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+          '@typescript-eslint/no-unsafe-argument': 'error',
+          '@typescript-eslint/no-unsafe-assignment': 'error',
+          '@typescript-eslint/no-unsafe-call': 'error',
+          '@typescript-eslint/no-unsafe-member-access': 'error',
+          '@typescript-eslint/no-unsafe-return': 'error',
+          'require-await': 'off',
+          '@typescript-eslint/require-await': 'error',
+          '@typescript-eslint/restrict-plus-operands': 'error',
+          '@typescript-eslint/restrict-template-expressions': 'error',
+          '@typescript-eslint/unbound-method': 'error',
+        },
+      }, {
+        // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/unbound-method.md
+        files: ['**/__tests__/**/*.ts', '**/*.spec.ts', '**/*.test.ts'],
+        plugins: ['jest'],
+        rules: {
+          // you should turn the original rule off *only* for test files
+          '@typescript-eslint/unbound-method': 'off',
+          'jest/unbound-method': 'error',
+        },
+      }],
   ),
   rules: {
-    'indent': 'off',
     'import/named': 'off',
-    'no-useless-constructor': 'off',
+
+    // TS
     '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
     '@typescript-eslint/member-delimiter-style': ['error', { multiline: { delimiter: 'none' } }],
     '@typescript-eslint/type-annotation-spacing': ['error', {}],
     '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', disallowTypeAnnotations: false }],
     '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
     '@typescript-eslint/prefer-ts-expect-error': 'error',
+
+    // Override JS
+    'no-useless-constructor': 'off',
+    'indent': 'off',
     '@typescript-eslint/indent': ['error', 2, {
       SwitchCase: 1,
       VariableDeclarator: 1,
@@ -106,8 +113,6 @@ module.exports = {
       ],
       offsetTernaryExpressions: true,
     }],
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'no-redeclare': 'off',
     '@typescript-eslint/no-redeclare': 'error',
     'no-use-before-define': 'off',
@@ -147,6 +152,8 @@ module.exports = {
     '@typescript-eslint/no-loss-of-precision': 'error',
     'lines-between-class-members': 'off',
     '@typescript-eslint/lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+
+    // off
     '@typescript-eslint/consistent-indexed-object-style': 'off',
     '@typescript-eslint/naming-convention': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
@@ -161,5 +168,7 @@ module.exports = {
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-namespace': 'off',
     '@typescript-eslint/triple-slash-reference': 'off',
+    // handled by unused-imports/no-unused-imports
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 }
