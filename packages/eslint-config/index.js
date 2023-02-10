69e6c014f654a935ad6676f32d58f03ffafdcabc
kaivanwong <kaivanwong@outlook.com>
2023-02-10 16:38:31 +0800

    test: only use vue

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -2,7 +2,6 @@ const { isPackageExists } = require('local-pkg')
 
 const Extends = []
 
-const TS = isPackageExists('typescript')
 const VUE = isPackageExists('vue')
 const REACT = isPackageExists('react')
 const NEST = isPackageExists('@nestjs/core')
@@ -17,5 +16,5 @@ if (NEST)
   Extends.push('@kaivanwong/eslint-config-nest')
 
 module.exports = {
-  extends: Extends,
+  extends: '@kaivanwong/eslint-config-vue',
 }
