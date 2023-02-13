6c395f4e3e8926db72d3aa90f122378fc177f669
kaivanwong <kaivanwong@outlook.com>
2023-02-13 16:03:42 +0800

    update package.json info

--- packages/all/index.js
+++ packages/all/index.js
@@ -1,5 +1,18 @@
+const { isPackageExists } = require('local-pkg')
+
+const VUE = isPackageExists('vue')
+const REACT = isPackageExists('react')
+
+const Extends = []
+
+if (VUE) {
+  Extends.push('@kaivanwong/eslint-config-vue')
+}
+
+if (REACT) {
+  Extends.push('@kaivanwong/eslint-config-react')
+}
+
 module.exports = {
-  extends: [
-    '@kaivanwong/eslint-config-vue',
-  ],
+  extends: Extends,
 }
