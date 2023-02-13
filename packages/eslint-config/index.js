00b56d4033fd4be39b9773c8849e3ad991857246
kaivanwong <kaivanwong@outlook.com>
2023-02-13 13:46:25 +0800

    only export vue

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,29 +1,3 @@
-const { isPackageExists } = require('local-pkg')
-
-const TS = isPackageExists('typescript')
-const VUE = isPackageExists('vue')
-const REACT = isPackageExists('react')
-const NEST = isPackageExists('@nestjs/core')
-
-let Extends = []
-
-if (TS) {
-  Extends = ['@kaivanwong/eslint-config-ts']
-}
-else {
-  Extends = ['@kaivanwong/eslint-config-base']
-  console.warn('[@kaivanwong/eslint-config] TypeScript is not installed, fallback to JS only.')
-}
-
-if (VUE)
-  Extends = ['@kaivanwong/eslint-config-vue']
-
-if (REACT)
-  Extends = ['@kaivanwong/eslint-config-react']
-
-if (NEST)
-  Extends = ['@kaivanwong/eslint-config-nest']
-
 module.exports = {
-  extends: Extends,
+  extends: '@kaivanwong/eslint-config-vue',
 }
