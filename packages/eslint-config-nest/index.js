12b1f7e66fcef96b356441f576269b28f430ec75
kaivanwong <kaivanwong@outlook.com>
2023-02-14 09:19:37 +0800

    add nest eslint config

---
+++ packages/eslint-config-nest/index.js
@@ -0,0 +1,17 @@
+const { isPackageExists } = require('local-pkg')
+
+const TS = isPackageExists('typescript')
+
+if (!TS)
+  console.warn('[@kaivanwong/eslint-config] TypeScript is not installed, fallback to JS only.')
+
+module.exports = {
+  extends: [
+    TS
+      ? '@kaivanwong/eslint-config-ts'
+      : '@kaivanwong/eslint-config-base',
+  ],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'off',
+  },
+}
