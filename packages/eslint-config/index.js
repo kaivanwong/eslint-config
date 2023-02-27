e7fceb64808d4710ba79179a73b6f322aee25da3
kaivanwong <kaivanwong@outlook.com>
2023-02-27 11:13:10 +0800

    update

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,26 +1,26 @@
 const { isPackageExists } = require('local-pkg')
 
-const TS = isPackageExists('typescript')
+// const TS = isPackageExists('typescript')
 const VUE = isPackageExists('vue')
 const REACT = isPackageExists('react')
 const NEST = isPackageExists('@nestjs/core') || isPackageExists('@nestjs/common') || isPackageExists('@nestjs/cli')
 
-let Extends = ['@kaivanwong/eslint-config-base']
+const Extends = []
 
-if (TS) {
-  Extends = ['@kaivanwong/eslint-config-ts']
-}
+// if (TS) {
+//   Extends = ['@kaivanwong/eslint-config-ts']
+// }
 
 if (VUE) {
-  Extends = ['@kaivanwong/eslint-config-vue']
+  Extends.push('@kaivanwong/eslint-config-vue')
 }
 
 if (REACT) {
-  Extends = ['@kaivanwong/eslint-config-react']
+  Extends.push('@kaivanwong/eslint-config-react')
 }
 
 if (NEST) {
-  Extends = ['@kaivanwong/eslint-config-nest']
+  Extends.push('@kaivanwong/eslint-config-nest')
 }
 
 module.exports = {
